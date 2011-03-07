d622540131a27695da04d8e0433eeec96b286a19
Laban Mwangi <laban@afrinic.net>
2011-03-07 15:50:55 +0400

    Graphing... and there was much celebration

--- awk/sorting/graph_performance.sh
+++ awk/sorting/graph_performance.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 RESULT_FILE="tmp/results.txt"
-GRAPH_IMAGE="./performance.png"
+GRAPH_IMAGE="tmp/performance.png"
+
+# Generate stats
 ./make_stats.sh > ${RESULT_FILE}
+
+# Separate the data into files
 cat ${RESULT_FILE} |sed -e "s/.* of \(\w\+\).awk.* \([0-9]\+\) ints.*\: \(.*\)/\1 \2 \3/g"|sort|grep bub > tmp/bsort.perf 
 cat ${RESULT_FILE} |sed -e "s/.* of \(\w\+\).awk.* \([0-9]\+\) ints.*\: \(.*\)/\1 \2 \3/g"|sort|grep qui > tmp/qsort.perf 
 cat ${RESULT_FILE} |sed -e "s/.* of \(\w\+\).awk.* \([0-9]\+\) ints.*\: \(.*\)/\1 \2 \3/g"|sort|grep sele > tmp/ssort.perf 
 
+# Graph
 
 echo """
 set terminal png;
-set output "$GRAPH_IMAGE";
+set output '$GRAPH_IMAGE';
 set xrange [0:];
-set title "Sort performance";
-set xlabel "Sort set size";
-set ylabel "Time in seconds";
-plot "./tmp/qsort.perf" using 2:3 smooth csplines title "Quick sort", "./tmp/ssort.perf" using 2:3 smooth csplines title "Selection sort", "./tmp/bsort.perf" using 2:3 smooth csplines title "Bubble sort"
+set title 'Sort performance';
+set xlabel 'Sort set size';
+set ylabel 'Time in seconds';
+plot './tmp/qsort.perf' using 2:3 smooth csplines title 'Quick sort', './tmp/ssort.perf' using 2:3 smooth csplines title 'Selection sort', './tmp/bsort.perf' using 2:3 smooth csplines title 'Bubble sort'
 """|gnuplot
 
